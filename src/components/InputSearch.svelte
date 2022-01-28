93ad9be96f9e57337f26262ad353d0561af2cfd7
iwannabekoshka <pokusaeff.daniil@gmail.com>
2022-01-28 15:12:27 +0300

    feat: add input component

---
+++ src/components/InputSearch.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let value
+	export let placeholder
+</script>
+
+<input
+	class='w-full rounded-md text--lg p-4 border-2 border-gray-200'
+	type='text'
+	placeholder={placeholder}
+	bind:value={value}
+>
